0c8bcb7ff4f9d1e2744d8bb2c514609966c2d65f
phonixx1 <sacharevol.1998@live.fr>
2020-04-10 22:46:28 +0200

    workpull

--- Bdd_Rezmer_Revol/WpfBdd/FenMenuCommande.xaml.cs
+++ Bdd_Rezmer_Revol/WpfBdd/FenMenuCommande.xaml.cs
@@ -153,42 +153,43 @@ namespace WpfBdd
             {
                 foreach( string elem in listChoix)
                 {
-                    commande.CommandText= "UPDATE recette set compteur = compteur+"+quantiteList[listChoix.IndexOf(elem)] +"where idRecette ="+ elem.Substring(0, 4) + ";";
+                    commande.CommandText= "UPDATE recette set compteur = compteur+"+quantiteList[listChoix.IndexOf(elem)] +" where idRecette ='"+ elem.Substring(0, 4) + "';";
                     commande.ExecuteNonQuery();
-                    commande.CommandText = "select compteur from recette where idRecette=" + elem.Substring(0, 4) + ";";
+                    commande.CommandText = "select compteur from recette where idRecette='" + elem.Substring(0, 4) + "';";
                     reader = commande.ExecuteReader();
                     reader.Read();
                     compteurRecette = reader.GetInt32(0);
                     reader.Close();
                     if (compteurRecette > 10 && compteurRecette - Convert.ToInt32(quantiteList[listChoix.IndexOf(elem)]) < 10)
                     {
-                        commande.CommandText= "UPDATE recette SET prixDeVente=prixDeVente+2  where idRecette=" + elem.Substring(0, 4) + ";";
+                        commande.CommandText= "UPDATE recette SET prixDeVente=prixDeVente+2  where idRecette='" + elem.Substring(0, 4) + "';";
                         commande.ExecuteNonQuery();
 
                     }
                     else if (compteurRecette > 50 && compteurRecette - Convert.ToInt32(quantiteList[listChoix.IndexOf(elem)]) < 50)
                     {
-                        commande.CommandText = "UPDATE recette SET prixDeVente=prixDeVente+5  where idRecette=" + elem.Substring(0, 4) + ";";
+                        commande.CommandText = "UPDATE recette SET prixDeVente=prixDeVente+5  where idRecette='" + elem.Substring(0, 4) + "';";
                         commande.ExecuteNonQuery();
 
                     }
                     
                     if (compteurRecette<50)
                     {
-                        commande.CommandText = "UPDATE client SET soldeCook=soldeCook+2 where idClient=" + MainWindow.IdCurrentClient + ";";
+                        commande.CommandText = "UPDATE client SET soldeCook=soldeCook+2 where idCompte='" + MainWindow.IdCurrentClient + "';";
                     }
                     else
                     {
-                        commande.CommandText = "UPDATE client SET soldeCook=soldeCook+4 where idClient=" + MainWindow.IdCurrentClient + ";";
+                        commande.CommandText = "UPDATE client SET soldeCook=soldeCook+4 where idCompte0='" + MainWindow.IdCurrentClient + "';";
                     }
                     commande.ExecuteNonQuery();
-                    commande.CommandText = "UPDATE produit natural join estconstitue natural join recette SET stockActuel=stockActuel-"+ quantiteList[listChoix.IndexOf(elem)] + "*estconstitue.quantiteUtilisee where recette.idRecette="+ elem.Substring(0, 4) + " and  recette.idRecette=estconstitue.idRecette and estconstitue.idProduit=produit.idProduit;";
+                    commande.CommandText = "UPDATE produit natural join estconstitue natural join recette SET stockActuel=stockActuel-"+ quantiteList[listChoix.IndexOf(elem)] + "*estconstitue.quantiteUtilisee where recette.idRecette='"+ elem.Substring(0, 4) + "' and  recette.idRecette=estconstitue.idRecette and estconstitue.idProduit=produit.idProduit;";
                     commande.ExecuteNonQuery();
 
 
 
                 }
                 MessageBox.Show("Commande Validée tout les produits sont disponibles\nRedirection vers notre site de payement sécurisé...");
+                this.Close();
 
             }
             else
